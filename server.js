45dfef2a233cf9096f70881d5d4e0db514c2cbd4
danieltorres31 <danieltorres31@outlook.com>
2020-04-12 20:34:30 -0300

    feat: Connect frontend and backend with websocket

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import express from 'express'
 import http from 'http'
+import socketio from 'socket.io'
+
+import createGame from './public/game.js'
 
 const app = express()
 const server = http.createServer(app)
+const socket = socketio(server)
 
 app.use(express.static('public'))
+
+const game = createGame()
+
+socket.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log(`Player connected with id: ${playerId}`)
+    socket.emit('setup', game.state)
+})
 
 const port = 3000
 server.listen(port, () => {
