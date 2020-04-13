f3dbdf970ae2b62d5f3cb62c142cc0c5bf91b354
danieltorres31 <danieltorres31@outlook.com>
2020-04-12 21:30:26 -0300

    feat: Implements observer to game actions

--- public/game.js
+++ public/game.js
@@ -8,21 +8,55 @@ export default function createGame() {
         }
     }
 
+    const observers = []
+
+    function subscribe(observerFunction) {
+        observers.push(observerFunction)
+    }
+
+    function notifyAll(command) {
+        observers.forEach(observerFunction => {
+            observerFunction(command)
+        })
+    }
+
+    function setState(newState) {
+        Object.assign(state, newState)
+    }
+
     function addPlayer(command) {
+        const playerX = 'playerX' in command ? command.playerX : Math.floor(Math.random() * state.screen.width)
+        const playerY = 'playerY' in command ? command.playerY : Math.floor(Math.random() * state.screen.height)
+
         state.players[command.playerId] = {
-            x: command.playerX,
-            y: command.playerY
+            x: playerX,
+            y: playerY
         }
+
+        notifyAll({
+            type: 'add-player',
+            playerId: command.playerId,
+            playerX,
+            playerY
+        })
     }
 
     function removePlayer(command) {
         delete state.players[command.playerId]
+        
+        notifyAll({
+            type: 'remove-player',
+            playerId: command.playerId,
+        })
     }
 
     function addFruit(command) {
+        const fruitX = 'fruitX' in command ? command.fruitX : Math.floor(Math.random() * state.screen.width)
+        const fruitY = 'fruitY' in command ? command.fruitY : Math.floor(Math.random() * state.screen.height)
+
         state.fruits[command.fruitId] = {
-            x: command.fruitX,
-            y: command.fruitY
+            x: fruitX,
+            y: fruitY
         }
     }
 
@@ -31,6 +65,8 @@ export default function createGame() {
     }
 
     function movePlayer(command) {
+        notifyAll(command)
+        
         const acceptedMoves = {
             ArrowUp(player) {
                 if (player.y - 1 >= 0)
@@ -72,6 +108,8 @@ export default function createGame() {
     }
 
     return {
+        setState,
+        subscribe,
         addPlayer,
         removePlayer,
         addFruit,
